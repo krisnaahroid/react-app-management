62682ba46b95e68d5291a87625a1703616a7aacf
Elga <elga.lesmana@gmail.com>
2017-11-21 21:08:52 +0700

    update logo

--- app/components/header/Brand.jsx
+++ app/components/header/Brand.jsx
@@ -3,7 +3,7 @@ import { NavbarBrand } from 'reactstrap'
 
 const Brand = () => (
   <NavbarBrand href="/">
-    <img src="/assets/logo/logo@2x.png" className="logo-2x" alt="kokoro logo" />
+    <img src="/assets/logo/logo.png" className="logo-2x" alt="kokoro logo" />
   </NavbarBrand>
 )
 
