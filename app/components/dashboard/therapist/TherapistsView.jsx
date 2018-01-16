ea6106b73b85a8b28d5803d8010a6db34ebf72ea
Elga <elga.lesmana@gmail.com>
2018-01-16 21:44:49 +0700

    therapist table

--- app/components/dashboard/therapist/TherapistsView.jsx
+++ app/components/dashboard/therapist/TherapistsView.jsx
@@ -1,31 +1,54 @@
 import PropTypes from 'prop-types'
 import { Table } from 'components/strap'
+import { startCase } from 'lodash'
+import StarRatingComponent from 'react-star-rating-component'
+
+const showAvatar = (image) => {
+  if (image.small_url) {
+    return (
+      <img src={image.small_url} className="avatar" alt="" />
+    )
+  }
+
+  return (<img src="/assets/avatar_no_image.png" className="avatar" alt="" />)
+}
 
 const TherapistsView = ({ therapists }) => (
-  <Table className="kokoro-table">
-    <thead>
-      <tr>
-        <th />
-        <th>Order</th>
-        <th>Income</th>
-        <th>Duration</th>
-        <th>Gender</th>
-      </tr>
-    </thead>
-    <tbody>
-      {
-        therapists.map((item, i) => (
-          <tr key={i}>
-            <td className="therapist">{item.attributes.name}</td>
-            <td>10</td>
-            <td>6.000.000</td>
-            <td>73 Minutes</td>
-            <td>{item.attributes.gender}</td>
-          </tr>
-        ))
-      }
-    </tbody>
-  </Table>
+  <div className="therapist-view-table">
+    <h1>Therapist</h1>
+
+    <Table className="kokoro-table">
+      <thead>
+        <tr>
+          <th />
+          <th>Order</th>
+          <th>Ratings</th>
+          <th>Income</th>
+          <th>Duration</th>
+          <th>Gender</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+            therapists.map((item, i) => (
+              <tr key={i}>
+                <td className="therapist">
+                  {showAvatar(item.attributes.avatar)}
+                  {item.attributes.name}
+                </td>
+                <td>{item.attributes.order}</td>
+                <td>
+                  <StarRatingComponent name="rate2" editing={false} starCount={5} value={item.attributes.rating} />
+                </td>
+                <td>{item.attributes.income}</td>
+                <td>{item.attributes.duration}</td>
+                <td>{startCase(item.attributes.gender)}</td>
+              </tr>
+            ))
+          }
+      </tbody>
+    </Table>
+  </div>
 )
 
 TherapistsView.propTypes = {
